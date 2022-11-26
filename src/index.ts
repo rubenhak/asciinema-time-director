f4b8d539450357dbbc24a1ae7e5268e00387027b
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-11-26 14:17:02 -0800

    fixes to ascii director

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
+import _ from 'the-lodash';
 import { Command } from 'commander';
 import { readFileSync, writeFileSync } from 'fs';
+
+
+const TERMINAL_LINE_CLEAR = "\u001b[2K";
+const TERMINAL_CARET_RETURN ="\u001b[1G";
+const TERMINAL_UNKNOWN_01 ="\u001b[?25h";
+const TERMINAL_UNKNOWN_02 ="\u001b[?25l";
+
+
 const program = new Command();
 
 program
@@ -19,66 +28,121 @@ program.command('slow-down')
 
     const contentsStr = readFileSync(options.in).toString();
     const lines = contentsStr.split('\n');
+    const objects: any[] = [];
 
-    const newLines : string[] = [];
+    for(const line of lines)
+    {
+      if (line.length > 0)
+      {
+        const obj = JSON.parse(line);
+        objects.push(obj);
+      }
+    }
+
+    const separatedObjects: any[] = [];
+    for(const obj of objects)
+    {
+      if (_.isArray(obj))
+      {
+        if (obj[1] === 'o')
+        {
+          const parts = separateString(
+            obj[2],
+            [
+              TERMINAL_LINE_CLEAR,
+              TERMINAL_CARET_RETURN,
+              TERMINAL_UNKNOWN_01,
+              TERMINAL_UNKNOWN_02
+            ]
+          );
+          for(const part of parts)
+          {
+            separatedObjects.push([obj[0], obj[1], part]);
+          }
+        }
+      }
+      else
+      {
+        separatedObjects.push(obj);
+      }
+    }
+
+    // for(const obj of separatedObjects)
+    // {
+    //   console.log(obj);
+    // }
+
+    const newObjects : any[] = [];
 
     let origLastTime : number = 0;
     let procLastTime : number = 0;
     let isNormalLine : boolean = true;
-    for(const line of lines)
+    for(const lineObject of separatedObjects)
     {
-      if (!shouldSkip(line)) {
-        newLines.push(convertLine(line));
-      }
-    }
-
-    const newContents = newLines.join('\n');
-    writeFileSync(options.out, newContents);
-
-    function convertLine(line: string)
-    {
-      if (line.startsWith('['))
+      if (_.isArray(lineObject))
       {
-        const commaIndex = line.indexOf(',');
-        if (commaIndex !== -1)
-        {
-          const timeStr = line.substring(1, commaIndex);
-          const time = parseFloat(timeStr);
-          const origDelta = time - origLastTime;
-          origLastTime = time;
-
-          const procDelta = decideDelta(line, origDelta);
-          procLastTime = procLastTime + procDelta;
-
-          line = `[${procLastTime.toFixed(6)}${line.substring(commaIndex)}`;
+        if (!shouldSkip(lineObject as any[])) {
+          newObjects.push(convertLine(lineObject as any[]));
         }
       }
-      return line;
+      else
+      {
+        newObjects.push(lineObject);
+      }
     }
 
-    function decideDelta(line: string, origDelta: number)
+    const newContents = 
+      newObjects.map(x => lineStringify(x))
+      .join('\n');
+    writeFileSync(options.out, newContents);
+
+    function convertLine(lineObject: any[0]) : any[]
     {
-      if (line.indexOf('[2K') !== -1) {
+      const time = lineObject[0] as number;
+      const origDelta = time - origLastTime;
+      origLastTime = time;
+
+      const procDelta = decideDelta(lineObject[2] as string, origDelta);
+      procLastTime = procLastTime + procDelta;
+
+      return [
+        procLastTime,
+        lineObject[1],
+        lineObject[2]
+      ];
+    }
+
+    function decideDelta(line: string, origDelta: number) : number
+    {
+      if (line === TERMINAL_LINE_CLEAR) {
         isNormalLine = false;
         return Math.max(origDelta, 0.02);
       }
-      if (line.indexOf('[1G') !== -1) {
+      if (line === TERMINAL_CARET_RETURN) {
         isNormalLine = false;
-        return origDelta;
+        return 0.001;
       }
 
       if (!isNormalLine) {
-        isNormalLine = true;  
-        return origDelta;
+        if (line === TERMINAL_UNKNOWN_01 || 
+            line === TERMINAL_UNKNOWN_02)
+        {
+            return 0.001;  
+        }
+      }
+
+      if (!isNormalLine) {
+        isNormalLine = true;
+        return 0.001;  
       }
 
       isNormalLine = true;
       return Math.max(origDelta, 0.05);
     }
 
-    function shouldSkip(line: string)
+    function shouldSkip(lineObject: any[])
     {
-      if (line.indexOf('Restored session:') !== -1) {
+      if (lineObject[2].indexOf('Restored session:') !== -1) {
         return true;
       }
       return false;
@@ -88,3 +152,33 @@ program.command('slow-down')
 program.parse();
 
 
+function separateString(line: string, separators: string[]) : string[]
+{
+  for(const separator of separators)
+  {
+    const index = line.indexOf(separator);
+    if (index !== -1)
+    {
+      return _.flatten([
+        separateString(line.substring(0, index), separators),
+        separator,
+        separateString(line.substring(index + separator.length), separators)
+      ]).filter(x => x.length > 0)
+    }
+  }
+
+  return [line];
+}
+
+function lineStringify(lineObject: any) : string
+{
+  if (_.isArray(lineObject))
+  {
+    const arr = lineObject as any[];
+    return `[${(arr[0] as number).toFixed(6)}, ${JSON.stringify(arr[1])}, ${JSON.stringify(arr[2])}]`
+  }
+  else
+  {
+    return JSON.stringify(lineObject);
+  }
+}
